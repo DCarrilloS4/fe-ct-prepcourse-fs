b75ccd67c2dfeb6294967b5da0a2ce29310284f6
DCarrilloS4 <danielcrl1996@gmail.com>
2024-04-29 21:42:00 -0500

    vamos mejorando demasiado, estamos subiendo de nivel, eso es importante, entendimos un poco mas sobre los parametros y la propiedad, seguimos ahora a integrar lo aprendido en objetos con lo aprendido en arreglos.

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -70,11 +70,10 @@ function tieneEmail(objetoUsuario) {
   // Verifica si el "objetoUsuario", en su propiedad "email", posee un valor definido.
   // En ese caso retornar true. Caso contrario, false.
   // Tu código:
-  var tieneEmail = objetoUsuario.hasOwnProperty("email");
-  if (tieneEmail === true) {
-    return true;
-  } else {
+  if (objetoUsuario.email === null || objetoUsuario.email === undefined) {
     return false;
+  } else {
+    return objetoUsuario.hasOwnProperty("email");
   }
 }
 
@@ -82,12 +81,18 @@ function tienePropiedad(objeto, propiedad) {
   // Verifica si el objeto recibido posee una propiedad con el mismo nombre que el parámetro "propiedad".
   // En ese caso retornar true. Caso contrario, false.
   // Tu código:
+  return objeto.hasOwnProperty(propiedad);
 }
 
 function verificarPassword(objetoUsuario, password) {
   // Verifica si la propiedad "password" del "objetoUsuario" coincide con el parámetro "password".
   // En ese caso retornar true. Caso contrario, false.
   // Tu código:
+  if (objetoUsuario.password === password) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function actualizarPassword(objetoUsuario, nuevaPassword) {
@@ -95,6 +100,8 @@ function actualizarPassword(objetoUsuario, nuevaPassword) {
   // La nueva contraseña la recibes por parámetro.
   // Retornar el objeto.
   // Tu código:
+  objetoUsuario.password = nuevaPassword;
+  return objetoUsuario;
 }
 
 function agregarAmigo(objetoUsuario, nuevoAmigo) {
